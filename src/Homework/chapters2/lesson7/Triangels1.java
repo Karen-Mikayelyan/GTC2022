988b7ce6c3e38fefb63c12da4945e4fc579e5941
Karen-Mikayelyan <mikkar89@mail.ru>
2022-05-13 21:41:42 +0400

    Figurepainter

---
+++ src/Homework/chapters2/lesson7/Triangels1.java
@@ -0,0 +1,55 @@
+package Homework.chapters2.lesson7;
+
+
+    public class Triangels1 {
+        public static void main(String[] args) {
+            /*    Figure 4
+
+             * * * * *
+               * * * *
+                 * * *
+                   * *
+                     *
+             * */
+            int height = 5;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = height; j > i; j--) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+
+            /*   Figure 5
+
+                *       3      
+               * *      2        
+              * * *     1  
+             * * * *    0
+              * * *    -1
+               * *     -2
+                *      -3
+
+             * */
+
+            for (int i = 3; i >= -3 ; i--) {
+                int y = i;
+                if(i < 0){
+                    y = -i;
+                }
+                for (int j = 3 - y; j < 4; j++) {
+                    System.out.println(" ");                    
+                }
+                for (int j = 0; j < ; j++) {
+                    
+                }
+            }
+           
+            }
+        }
+
+    
+
+
