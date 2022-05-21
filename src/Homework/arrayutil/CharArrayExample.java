1656247d4fb24daef6c781dc14d4cb8735db94a1
Karen-Mikayelyan <mikkar89@mail.ru>
2022-05-21 21:08:27 +0400

    CharArrayExample.new

--- src/Homework/arrayutil/CharArrayExample.java
+++ src/Homework/arrayutil/CharArrayExample.java
@@ -18,7 +18,7 @@ public class CharArrayExample {
 
         // 2․ Տպել մեջտեղի 2 սիմվոլները
         char[] chars1 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
-        System.out.print("middle two simvols : " + chars1[chars1.length / 2 - 1] + " " + chars1[chars1.length / 2]);
+        System.out.print("middle two symbols : " + chars1[chars1.length / 2 - 1] + " " + chars1[chars1.length / 2]);
         System.out.println();
 
         // 3․ տպել true, եթե մասիվը վերջանում է ly -ով, եթե ոչ՝ false
@@ -38,11 +38,12 @@ public class CharArrayExample {
 
 
         // 4․ Տպել true , եթե տեքստը պարունակում է bob բառը, բայց o -ի տեղը կարող է լինել ցանկացած սինվոլ։
-        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
+        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'b'};
         // օր․ abcbob -> true, b9b -> true, բայց bac -> false
         for (int i = 0; i < bobArray.length; i++) {
             if(bobArray[i] == 'b' && bobArray[i + 2] == 'b'){
                 System.out.println(true);
+                break;
             }
         }
 
