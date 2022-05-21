83900c4bd6b64764a9c6a77380f1fc2bb72fd846
Karen-Mikayelyan <mikkar89@mail.ru>
2022-05-21 18:59:00 +0400

    CharArrayExampleю

--- src/Homework/arrayutil/CharArrayExample.java
+++ src/Homework/arrayutil/CharArrayExample.java
@@ -18,30 +18,24 @@ public class CharArrayExample {
 
         // 2․ Տպել մեջտեղի 2 սիմվոլները
         char[] chars1 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
-        for (int i = 0; i < chars1.length; i++) {
-        }
         System.out.print("middle two simvols : " + chars1[chars1.length / 2 - 1] + " " + chars1[chars1.length / 2]);
         System.out.println();
 
         // 3․ տպել true, եթե մասիվը վերջանում է ly -ով, եթե ոչ՝ false
         char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
         boolean b = true;
-        for (int i = chars2.length - 2; i < chars2.length; i++) {
             if (chars2[chars2.length - 2] == 'l' && chars2[chars2.length - 1] == 'y') {
                 b = true;
             } else {
                 b = false;
             }
-        }
         System.out.println(b);
 
         // 3 -րդ խնդրի լուծման այլ տարբերակ
         char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
-        for (int i = chars3.length - 2; i < chars3.length; i++) {
-            boolean b1 = chars3[chars2.length - 2] == 'l' && chars3[chars2.length - 1] == 'y' ? true : false;
-            i++;
+            boolean b1 = chars3[chars3.length - 2] == 'l' && chars3[chars3.length - 1] == 'y' ? true : false;
             System.out.println(b1);
-        }
+
 
         // 4․ Տպել true , եթե տեքստը պարունակում է bob բառը, բայց o -ի տեղը կարող է լինել ցանկացած սինվոլ։
         char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
